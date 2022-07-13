a966cfcc5b3b9f11cd313908aae47f9dbe60f508
afarah45 <afarah45@gmail.com>
2022-07-13 18:37:18 -0500

    Add files via upload

---
+++ password_generator2.py
@@ -0,0 +1,13 @@
+#Password Generator Project
+import random
+chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '#', '$', '%', '&', '(', ')', '*', '+', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+print("Welcome to my Password Generator!")
+r_chars = int(input("How many characters would you like in your password?\n"))
+
+password = ""
+for rpass in range(1, r_chars + 1):
+    random_chars = random.choice(chars)
+    password += random_chars
+print(password)
+
